3b97e6515a94295b0cc544a4bdf5df0bac9fe9ff
PRAVIN KUMAR <dollykumari.iac@gmail.com>
2023-08-03 21:15:31 +0530

    Sample class is committed.

---
+++ src/test/java/Sample/Sample1.java
@@ -0,0 +1,14 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Sample1
+{
+	// TODO Auto-generated method stub
+@Test
+public void sample()
+{
+ System.out.println("hi gthis my sample class");
+}
+
+}
